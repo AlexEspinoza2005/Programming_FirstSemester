48063eebd81ff8097e42375f7911cead80cdad81
AlexEspinoza2005 <alexespinozacangas2018@gmail.com>
2025-02-26 14:39:47 -0500

    Añadiendo 01_DataTypes_Ej1

---
+++ 01_DataTypes_Ej1/data_types_ej1/src/main/java/com/datatype/Main.java
@@ -0,0 +1,9 @@
+package com.datatype;
+
+public class Main {
+
+    //Exercise 1: Display the string "Hello, World!" on the screen.
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
