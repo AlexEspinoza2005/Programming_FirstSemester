fb58ea4eb8dc1b3eb9e3118a43971d63e2158822
AlexEspinoza2005 <alexespinozacangas2018@gmail.com>
2025-02-28 16:01:58 -0500

    Añadiendo 02_DataTypes_Ej2

---
+++ 02_DataTypes_Ej2/data_types_ej2/src/main/java/com/datatype/Main.java
@@ -0,0 +1,9 @@
+package com.datatype;
+
+public class Main {
+    //Exercise 2: Store the string "Hello, World!" in a variable and display it.
+    public static void main(String[] args) {
+        String text = "Hello World!";
+        System.out.println(": "+text);
+    }
+}
